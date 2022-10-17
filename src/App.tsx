42ba2d09395742e4a18641a189e5a7f3e9200717
dwisabrina23 <dwisabrina10@gmail.com>
2022-10-17 17:46:26 +0800

    chore: add parcel, eslint setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import firebase from 'firebase/compat/app'
+import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage'
+
+import { storage } from './firebase'
 
 function App() {
+  const [imgUrl, setImgUrl] = useState<string>('')
+  const [progresspercent, setProgresspercent] = useState(0)
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    const file = e.target[0]?.files[0]
+    if (!file) return
+    const storageRef = ref(storage, `files/${file.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, file)
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+        setProgresspercent(progress)
+      },
+      (error) => {
+        alert(error)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgUrl(downloadURL)
+        })
+      },
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleSubmit} className='form'>
+        <input type='file' />
+        <button type='submit'>Upload</button>
+      </form>
+      {!imgUrl && (
+        <div className='outerbar'>
+          <div className='innerbar' style={{ width: `${progresspercent}%` }}>
+            {progresspercent}%
+          </div>
+        </div>
+      )}
+      {imgUrl && <img src={imgUrl} alt='uploaded file' height={200} />}
+      <p>{imgUrl}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app'
+import { getStorage } from 'firebase/storage'
+
+import {
+  API_KEY,
+  APP_ID,
+  AUTH_DOMAIN,
+  MEASUREMENT_ID,
+  MESSAGING_SENDER_ID,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+} from './constants/firebaseConstants'
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const storage = getStorage(app)
